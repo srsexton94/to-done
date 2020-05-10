69f47d242fb2bb701b071dab5cc0123fe29dec4b
srsexton94 <srsexton94@gmail.com>
2020-05-10 19:36:54 -0400

    Refactor code from on load function to new doc
    Create storage.js to refactor code from app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,27 +1,7 @@
 'use strict'
 
-const todosTemplate = require('./templates/todos.handlebars')
-
-// const add = () => {
-//   const myDate = new Date()
-//   const obj = {
-//     title: 'My 2nd todo item',
-//     date: myDate
-//   }
-//   const strObj = JSON.stringify(obj)
-//   const id = 'todo' + (localStorage.length + 1).toString()
-//   localStorage.setItem(id, strObj)
-// }
-
-const logStore = () => {
-  const storageArr = Object.values(localStorage).map(str => {
-    return JSON.parse(str)
-  })
-  const todosHtml = todosTemplate({ todos: storageArr })
-  $('#list').html(todosHtml)
-  console.log(todosHtml)
-}
+const storage = require('./storage.js')
 
 $(() => {
-  logStore()
+  storage.loadList()
 })
---
+++ assets/scripts/storage.js
@@ -0,0 +1,25 @@
+'use strict'
+const todosTemplate = require('./templates/todos.handlebars')
+
+// const add = () => {
+//   const myDate = new Date()
+//   const obj = {
+//     title: 'My 2nd todo item',
+//     date: myDate
+//   }
+//   const strObj = JSON.stringify(obj)
+//   const id = 'todo' + (localStorage.length + 1).toString()
+//   localStorage.setItem(id, strObj)
+// }
+
+const loadList = () => {
+  const storageArr = Object.values(localStorage).map(str => {
+    return JSON.parse(str)
+  })
+  const todosHtml = todosTemplate({ todos: storageArr })
+  $('#list').html(todosHtml)
+}
+
+module.exports = {
+  loadList
+}
